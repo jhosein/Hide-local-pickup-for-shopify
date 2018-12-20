abb1ffa07dce0b6de10ce3e5625624664074c30f
Jonathan Hosein <jphosein42@students.tntech.edu>
2018-12-20 01:13:19 -0600

    add code

---
+++ hide.js
@@ -0,0 +1,37 @@
+//only run if we are in the checkout page
+if(typeof Checkout === 'object'){
+    if(typeof Checkout.$ === 'function'){
+        console.log("live");
+
+        //attach even listener to continue button
+        let el = window.document.getElementsByClassName("step__footer__continue-btn");
+        el[0].addEventListener("click",function() {
+            let zip = window.document.getElementById("checkout_shipping_address_zip").value;
+            window.document.cookie = "zipCode=" + zip;
+        })
+
+
+        function getCookieValue(a) {
+            var b = document.cookie.match('(^|;)\\s*' + a + '\\s*=\\s*([^;]+)');
+            return b ? b.pop() : '';
+        }
+
+        //gets current US State if applicaple
+        let currentZip = getCookieValue("zipCode");
+        let isApproved = false;
+        let approvedZips = ['38501','38502','38503','38506','38583','38574','38570'];
+        for (let i = 0; i < approvedZips.length; i++) {
+            if(currentZip === approvedZips[i]) {
+                isApproved = true;
+            }
+        }
+        //deselects local picup if not approved zipcode
+        if (!isApproved)
+        {
+            let firstShipMethod = window.document.getElementById("checkout_shipping_rate_id_shopify-customer20pickup20cookeville20tn-000");
+            let shipMethods = window.document.getElementsByClassName('input-radio');
+            shipMethods[1].checked = true;
+            firstShipMethod.disabled= true;
+        }
+        }
+      }
